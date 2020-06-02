99dff4a1693b1fa29b114182292886e9128476a8
Huai <huai@melody.com.tw>
2020-06-02 13:50:48 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
